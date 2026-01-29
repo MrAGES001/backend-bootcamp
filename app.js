a7841c29fd4f7d3d383dbeca27c7d43f6995cf15
MrAGES001 <sakyiages@gmail.com>
2026-01-29 19:35:18 +0000

    Remove setup endpoint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const usersRouter = require("./routes/users.routes");
 const authRouter = require("./routes/auth.routes");
 const meRouter = require("./routes/me.routes");
 const adminRouter = require("./routes/admin.routes");
-const { cloudSetup } = require("./db/cloudSetup");
+
 
 const { notFound, errorHandler } = require("./middleware/errorHandler");
 
@@ -29,19 +29,7 @@ app.use("/auth", authRouter);
 app.use("/me", meRouter);
 app.use("/admin", adminRouter);
 
-// TEMPORARY: run once then delete
-app.get("/__setup", async (req, res) => {
-  try {
-    const message = await cloudSetup();
-    res.json({ message });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      error: "Cloud DB setup failed",
-      detail: err.message
-    });
-  }
-});
+
 
 // error handlers (must be last)
 app.use(notFound);
